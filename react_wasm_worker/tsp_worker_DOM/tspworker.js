eeb998c45b7097a6b590f4f972757435cbf79fb0
Joy Hughes <42107664+joyhughes@users.noreply.github.com>
2024-10-18 18:12:02 -0700

    Further update DOM examples

--- react_wasm_worker/tsp_worker_DOM/tspworker.js
+++ react_wasm_worker/tsp_worker_DOM/tspworker.js
@@ -3,29 +3,25 @@ import Module from './tsp.js';
 let tspModule = null;
 
 Module().then((initializedModule) => {
-    tspModule = initializedModule;
-    console.log("Emscripten Module Initialized"); // Log to verify initialization
+  tspModule = initializedModule;
+  console.log("Emscripten Module Initialized");
 
-    // Listen for messages from the main thread
-    self.addEventListener('message', (event) => {
-        console.log("Worker received a message:", event.data); // Log to verify message reception
+  self.addEventListener('message', (event) => {
+    if (event.data.action === 'runTSP') {
+      const numPoints = event.data.numPoints;
+      try {
+        const callback = (interimJson) => {
+          // Send interim points to the main thread
+          self.postMessage({ action: 'interimTSP', result: interimJson });
+        };
 
-        if (event.data.action === 'runTSP') {
-            const numPoints = event.data.numPoints;
-            try {
-                if (!tspModule) {
-                    throw new Error("Module not initialized");
-                }
-                console.log("Running TSP with numPoints:", numPoints); // Before running TSP
-                const result = tspModule.runTSP(numPoints);
-                console.log("TSP computation finished with result:", result); // After running TSP
-                self.postMessage({ action: 'tspResult', result: result });
-            } catch (error) {
-                console.error("Error running TSP:", error);
-                self.postMessage({ action: 'tspError', message: error.message });
-            }
-        }
-    });
-}).catch((err) => {
-    console.error("Failed to initialize the module:", err);
+        const result = tspModule.runTSP(numPoints, callback);
+        // Send the final result to the main thread
+        self.postMessage({ action: 'tspResult', result: result });
+      } catch (error) {
+        console.error("Error running TSP:", error);
+        self.postMessage({ action: 'tspError', message: error.message });
+      }
+    }
+  });
 });
